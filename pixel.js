b94a6a7268858deb7242609c2e96b9c7ac38e242
Edison <edisontoole@gmail.com>
2018-05-18 16:24:11 -0600

    Didn't realize the last build wasnt even working. works now

--- pixel.js
+++ pixel.js
@@ -1,18 +1,18 @@
 document.addEventListener("DOMContentLoaded", function() {
-  let color;
   console.log('DOM is on');
+
+  let color = document.getElementsByClassName("container")[0];
   let pixel = document.getElementsByClassName("grid-container")[0];
 
 
   pixel.addEventListener("click", function(e){
+    console.log("clicked pix");
     e.target.style.backgroundColor = color
 
   });
 
-  let shit = document.getElementsByClassName("pallette")[0];
 
-
-  shit.addEventListener("click", function(e){
+  color.addEventListener("click", function(e){
     console.log(e.target.id);
     color = e.target.id;
   });
