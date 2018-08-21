905063ac7ed7d27a9da6dac5abfd07fcbdb97bcc
Edison <edisontoole@gmail.com>
2018-08-21 13:54:22 -0600

    added conditions to make colors go back to white when clicked, and added preventDefaults to events.

--- pixel.js
+++ pixel.js
@@ -6,13 +6,19 @@ document.addEventListener("DOMContentLoaded", function() {
 
 
   pixel.addEventListener("click", function(e){
-    console.log("clicked pix");
+    e.preventDefault()
+    if(e.target.style.backgroundColor === "blue" || e.target.style.backgroundColor === "red" || e.target.style.backgroundColor === "pink"
+    || e.target.style.backgroundColor === "black" || e.target.style.backgroundColor === "orange" || e.target.style.backgroundColor === "purple"){
+      e.target.style.backgroundColor = "white"
+    }
+    else{
     e.target.style.backgroundColor = color
-
+  }
   });
 
 
   color.addEventListener("click", function(e){
+    e.preventDefault()
     console.log(e.target.id);
     color = e.target.id;
   });
